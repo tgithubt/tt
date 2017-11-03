c676e9a51c81e79133c2f7767c2c1147aa52189c
tgithubt <1297015725@qq.com>
2017-11-03 19:08:48 +0800

    first modify

--- src/tt/Ttest.java
+++ src/tt/Ttest.java
@@ -5,6 +5,9 @@ public class Ttest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hi git");
+		
+		int a = 90;
+		System.out.println(a);
 	}
 
 }
